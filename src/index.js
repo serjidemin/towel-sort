1d00fefe36569abcfe12b3a6891014fe5813abfb
Serji Demin <serji2003@rambler.ru>
2020-10-06 23:45:44 +0500

    feat: add function to sort the matrix

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (matrix == undefined) {
+        return [];
+    }
+    let arr = [];
+    matrix.forEach(function(item, i) {
+        if (i > 0 && i % 2 != 0) {
+            arr.push(item.reverse());
+        } else {
+            arr.push(item);
+        }
+    });
+    return arr.flat();
 }
